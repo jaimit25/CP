18a02212c09955064a490219faaf16404c1d1310
Jaimit <jaimit.jp@gmail.com>
2022-09-07 17:09:06 +0530

    had question

--- test.cpp
+++ test.cpp
@@ -1,77 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
-class TreeNode
-{
+class TreeNode {
 public:
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
-TreeNode *insertListLevelOrder(vector<int> data)
-{
-	TreeNode *temp, *root, *node;
-	queue<TreeNode *> q;
-	int i = 0;
-	root = new TreeNode(data[i]);
-	q.push(root);
-	i++;
+TreeNode *insertListLevelOrder(vector<int> data) {
+  TreeNode *temp, *root, *node;
+  queue<TreeNode *> q;
+  int i = 0;
+  root = new TreeNode(data[i]);
+  q.push(root);
+  i++;
 
-	while (q.size() > 0 && i < data.size())
-	{
-		node = q.front();
-		q.pop();
-		// left
-		if (data[i] != -1)
-		{
-			node->left = new TreeNode(data[i]);
-			q.push(node->left);
-		}
-		i++;
-		// right
-		if (data[i] != -1)
-		{
-			node->right = new TreeNode(data[i]);
-			q.push(node->right);
-		}
-		i++;
-	}
-	return root;
+  while (q.size() > 0 && i < data.size()) {
+    node = q.front();
+    q.pop();
+    // left
+    if (data[i] != -1) {
+      node->left = new TreeNode(data[i]);
+      q.push(node->left);
+    }
+    i++;
+    // right
+    if (data[i] != -1) {
+      node->right = new TreeNode(data[i]);
+      q.push(node->right);
+    }
+    i++;
+  }
+  return root;
 }
-void print_preorderTraversal(TreeNode *root)
-{
-	if (root)
-	{
-		cout << root->val << " ";
-		print_preorderTraversal(root->left);
-		print_preorderTraversal(root->right);
-	}
+void print_preorderTraversal(TreeNode *root) {
+  if (root) {
+    cout << root->val << " ";
+    print_preorderTraversal(root->left);
+    print_preorderTraversal(root->right);
+  }
 }
 
-void print_inorderTraversal(TreeNode *root)
-{
-	if (root)
-	{
-		print_inorderTraversal(root->left);
-		cout << root->val << " ";
-		print_inorderTraversal(root->right);
-	}
+void print_inorderTraversal(TreeNode *root) {
+  if (root) {
+    print_inorderTraversal(root->left);
+    cout << root->val << " ";
+    print_inorderTraversal(root->right);
+  }
 }
-void print_postorderTraversal(TreeNode *root)
-{
-	if (root)
-	{
-		print_postorderTraversal(root->left);
-		print_postorderTraversal(root->right);
-		cout << root->val << " ";
-	}
+void print_postorderTraversal(TreeNode *root) {
+  if (root) {
+    print_postorderTraversal(root->left);
+    print_postorderTraversal(root->right);
+    cout << root->val << " ";
+  }
 }
 
-int main()
-{
+int main() { return 0; }
 
-	return 0;
+bool isPrime(int n){
+  int i, flag = 0;
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag == 0)
+    return true;
+  else
+    return false;
 }
