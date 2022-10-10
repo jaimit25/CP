eae896b25ce4e609da7b2e3cf3d49ece4747c76b
Jaimit <jaimit.jp@gmail.com>
2022-10-10 20:45:03 +0530

    Graph 10/10/22 Detect Cycle

---
+++ Graphs(S)/8_detectCycleDfs.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+  int flag = 0;
+
+  void dfs(int v, int parent, vector<int> &vist, vector<int> adj[]) {
+    vist[v] = 1;
+    for (auto u : adj[v]) {
+      if (vist[u] == 0) {
+        dfs(u, v, vist, adj);
+      }
+      else if (u != parent) {
+        flag = 1;
+      }
+    }
+  }
+
+  bool isCycle(int V, vector<int> adj[]) {
+    vector<int> vist(V, 0);
+    for (int i = 0; i < V; i++) {
+      if (vist[i] == 0) {
+        dfs(i, -1, vist, adj);
+      }
+    }
+    if (flag == 1)
+      return true;
+    return false;
+  }
+};
+
+//{ Driver Code Starts.
+int main() {
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++) {
+      int u, v;
+      cin >> u >> v;
+      adj[u].push_back(v);
+      adj[v].push_back(u);
+    }
+    Solution obj;
+    bool ans = obj.isCycle(V, adj);
+    if (ans)
+      cout << "1\n";
+    else
+      cout << "0\n";
+  }
+  return 0;
+}
+// } Driver Code Ends
