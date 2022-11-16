83b0b4a4b5f9c8a848c0f0036fa1e686df60d950
Jaimit <jaimit.jp@gmail.com>
2022-11-16 19:46:06 +0530

    psh updated | Wed 16 Nov 2022 19:46:06 IST

--- psh.sh
+++ psh.sh
@@ -2,15 +2,18 @@
 function psh(){
 	echo "Enter Message :"
 	read mssg
+	dt=$(date)
+	echo "$dt"
 	if [ -z "$mssg" ] 
 	then  
 		echo "message cannot be empty" 
 	else 	
 		git add .
-		git commit -m"$mssg"
+		git commit -m"${mssg} | $dt "
 		git push
 		echo "Pushed to CP Repository :)" 
 	fi 
 }
 
 
+psh
