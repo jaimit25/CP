41e14bf0345bbc0dc7fe52351353dcc91806baec
jaimit25 <jaimit.jp@gmail.com>
2023-08-06 17:25:19 +0530

    max dist btn 2 elements | Sun Aug  6 17:25:19 IST 2023

--- test.cpp
+++ test.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 void solve()
 {
-    vi ps = {1,2,3};
-    
+
+    // // vi a = {1,1,2,2,2,1};
+    // vi a = {3,2,1,2,1,4,5,8,6,7,4,2};
+    vi a = {1};
+
+    int maxDist = 0;
+    unordered_map<int,int> mp; //{ele,ind}
+
+    for(int i = 0 ; i < a.size(); i++){
+        if(mp.find(a[i]) == mp.end()){
+            //first time seen
+            mp[a[i]] = i;
+        }else{
+            maxDist = max(maxDist,i-mp[a[i]]);
+        }
+    }
+    cout<<maxDist;
 }
  
 int main()
