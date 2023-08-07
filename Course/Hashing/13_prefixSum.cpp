a7ed4efadba669d17c56ae86e60466b0c9de6afa
jaimit25 <jaimit.jp@gmail.com>
2023-08-07 21:37:43 +0530

    largest subArray with 0 sum | Mon Aug  7 21:37:43 IST 2023

---
+++ Course/Hashing/13_prefixSum.cpp
@@ -0,0 +1,51 @@
+
+#include<bits/stdc++.h>
+#define vi vector<int>
+
+using namespace std;
+
+
+ int maxLen(vector<int>&a, int n)
+    {   
+        unordered_map<int,int> mp; // {prefixSum , indexOfFirstoccurence_of_ps}
+        int maxlen = 0 ; 
+        int sum = 0 ; 
+        for(int i = 0;  i < n ; i++){
+            sum += a[i];
+            if(sum == 0 ) maxlen = max(maxlen,i+1);
+            else{
+                if(mp.find(sum) != mp.end()){
+                    maxlen = max(maxlen, i - mp[sum]);
+                }else{
+                    mp[sum]= i;
+                }
+            }
+        }
+        return maxlen;
+    }
+
+
+void solve()
+{
+	// ps[r] = ps[l-1] + sum[l...r];
+	
+	vi a = {15,-2,2,-8,1,7,10,23};
+	cout<<maxlen(a,a.size());
+
+}
+ 
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+solve();
+ 
+return 0;
+}
+
+
+
--- test.cpp
+++ test.cpp
@@ -6,37 +6,8 @@ using namespace std;
 
 void solve()
 {
-    
-    unordered_map<char,int> minf;
-    vector<string> words = {"bella","label","roller"};
 
-    //we will use minf to get the number of char repeatations in all the words together.
-    for(char ch = 'a' ; ch <= 'z' ; ch++){
-        minf[ch] = 1000;
-    }
 
-    for(auto it :words){
-        unordered_map<char,int> mp;
-        for(auto c : it)
-            mp[c]++;
-        for(char ch = 'a' ; ch<= 'z' ; ch++){
-            minf[ch] = min(minf[ch],mp[ch]);
-        }
-    }
-
-    vector<string> ans;
-
-    for(char ch = 'a' ; ch<= 'z' ; ch++){
-        while(minf[ch]){
-            ans.push_back(string(1,ch));
-            minf[ch]--;
-        }
-    }
-    // return ans;
-
-    for(auto it:ans){
-        cout<<it<<" ";
-    }
 }
  
 int main()
